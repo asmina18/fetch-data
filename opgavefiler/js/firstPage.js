62195a1a33ae04baa166413ccce10b82ec39b8b7
Amina <113165708+asmina18@users.noreply.github.com>
2023-03-03 13:35:57 +0100

    opgave 1

--- opgavefiler/js/firstPage.js
+++ opgavefiler/js/firstPage.js
@@ -1,8 +1,43 @@
+
+
 /* Opgave 1*/
 // din kode her
 
 const userURI = "https://jsonplaceholder.typicode.com/users";
+
 const myUserlistElement = document.getElementById("userList");
+ 
+
+// mi berem data s fetch
+ fetch (userURI)
+//then eto kogda ti sdelala fetch  togda ti idesh na .Then potom indesh na drugoy .Then 
+ .then(
+   (myResponse)=> {
+       return myResponse.json();
+   }
+ )
+.then (
+     (data) =>{
+        console.log('   Her er min data: ', data);
+        buildUsers(data);
+     }
+)
+//finder fejl tidligt/ i ishesh oshibku  s nachinaya s Fetch
+
+.catch(  
+        (error) => {
+            console.error(error);
+       
+ });
+
+function buildUsers(myUserData){
+    myUserData.map((myUser)=>{
+    let myUserHTML=`<h2>${myUser.name}</h2>
+
+    <p>Adresse: ${myUser.address.street}  ${myUser.address.suite}</br>post nummer: ${myUser.address.zipcode}</br>by: ${myUser.address.city}</p>`;
+    myUserlistElement.innerHTML+=myUserHTML;
+    })
+};
 
 
 /* Opgave 2*/
